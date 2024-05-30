9673be5f7ef33d94c0f8fab391a7e188129e0738
Mariam Kajo <Maryamkajo2001@hotmail.com>
2024-05-30 14:17:28 +0200

    Styled rating screen 2
    Styling and adding stars using fontawesomicons

--- frontend/src/components/RatingScreen2.js
+++ frontend/src/components/RatingScreen2.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './css/RatingScreen2.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as solidStar, faStar as regularStar } from '@fortawesome/free-solid-svg-icons';
 
 const RatingScreen2 = ({ onComplete }) => {
   const [rating, setRating] = useState(0);
@@ -17,7 +19,7 @@ const RatingScreen2 = ({ onComplete }) => {
         <span className="user-label">Player</span>
       </div>
       <h1 className="title">LOADING</h1>
-      <h2>Spørsmål2?</h2>
+      <h2 className='question'>Theater content?</h2>
       <div className="stars">
         {[...Array(5)].map((star, index) => {
           index += 1;
@@ -29,7 +31,7 @@ const RatingScreen2 = ({ onComplete }) => {
               onMouseEnter={() => setHover(index)}
               onMouseLeave={() => setHover(rating)}
             >
-              x
+              <FontAwesomeIcon icon={index <= (hover || rating) ? solidStar : regularStar} /> 
             </span>
           );
         })}
