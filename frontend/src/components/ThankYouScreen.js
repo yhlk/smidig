e85b7bfb3e2225a377ad7fdb888486b67029ed14
Patanakarun <robinobs@hotmail.com>
2024-05-30 14:27:54 +0200

    Decisionscreen og loadingscreen og thankyouscreen
    håkon sin loading og thankyouscreen

--- frontend/src/components/ThankYouScreen.js
+++ frontend/src/components/ThankYouScreen.js
@@ -4,11 +4,8 @@ import './css/ThankYouScreen.css';
 const ThankYouScreen = ({ onReview }) => {
   return (
     <div className="thankyou-screen">
-      <div className="header">
-        <span className="user-icon">'ikon'</span>
-        <span className="user-label">Player</span>
-      </div>
-      <h1 className="title">Review</h1>
+      <h1 className="title">Thank you for playing!</h1>
+      <h2 className="subtitle">Please rate your experience</h2> 
       <button className="review-button" onClick={onReview}>Review</button>
     </div>
   );
