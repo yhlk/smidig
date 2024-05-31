f4c4ce27b4fcf3e40fdd1670661f725cda63f299
yhlk <123506602+yhlk@users.noreply.github.com>
2024-05-31 12:53:27 +0200

    Revert "Maryam"

--- frontend/src/components/DecisionScreen.js
+++ frontend/src/components/DecisionScreen.js
@@ -1,21 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './css/DecisionScreen.css';
-import untilImage from '../until.png'; // Adjust the path according to your project structure
 
 const DecisionScreen = ({ onDecisionComplete }) => {
-  const [timeLeft, setTimeLeft] = useState(100);
-
-  useEffect(() => {
-    if (timeLeft > 0) {
-      const timerId = setInterval(() => {
-        setTimeLeft(timeLeft - 1);
-      }, 1000);
-      return () => clearInterval(timerId);
-    } else {
-      onDecisionComplete();
-    }
-  }, [timeLeft, onDecisionComplete]);
-
   const handleOptionClick = () => {
     onDecisionComplete();
   };
@@ -23,26 +9,12 @@ const DecisionScreen = ({ onDecisionComplete }) => {
   return (
     <div className="decision-screen">
       <div className="header">
-        <span className="user-icon">🔹</span>
+        <span className="user-icon">'ikon'</span>
         <span className="user-label">Player</span>
       </div>
-      <h1>What should Magnus do next?</h1>
+      <h1>Question</h1>
       <div className="options-container">
-        <div className="left-options">
-          <button className="option-button purple" onClick={handleOptionClick}>HIDE</button>
-          <button className="option-button red" onClick={handleOptionClick}>KILL</button>
-        </div>
-        <div className="image-placeholder">
-          <img src={untilImage} alt="What should Magnus do next decision screen" className="decision-image" />
-        </div>
-        <div className="right-options">
-          <button className="option-button blue" onClick={handleOptionClick}>DO NOTHING</button>
-          <button className="option-button green" onClick={handleOptionClick}>LEAVE</button>
-        </div>
-      </div>
-      <div className="timer">
-        <div className="clock"></div>
-        Time left: <span id="timer">{timeLeft < 10 ? `0${timeLeft}` : timeLeft}</span> sec
+        <button className="option-button hide" onClick={handleOptionClick}>option1</button>
       </div>
     </div>
   );
