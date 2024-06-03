cf528c01de074e8e6aad36fa523dd5afc69ff97c
Mariam Kajo <Maryamkajo2001@hotmail.com>
2024-06-03 10:53:02 +0200

    css chnage

--- frontend/src/components/ThankYouScreen.js
+++ frontend/src/components/ThankYouScreen.js
@@ -3,20 +3,15 @@ import './css/ThankYouScreen.css';
 
 const ThankYouScreen = ({ onReview }) => {
   return (
-    <>
-      <div className="thankyou-screen">
-        <div className="header">
-          <h1 className='H1'> Thank you for playing!</h1>
-          <h1 className='MM'>please rate your</h1>
-          <h1 className='H3'>experience</h1>
-        </div>
-        <button className="review-button" onClick={onReview}>Review</button>
-      </div>
-      <div className="Player-icon">
+    <><div className="thankyou-screen">
+      <h1 className="title">Thank you for playing!</h1>
+      <h2 className="subtitle">Please rate your experience</h2>
+      <button className="review-button" onClick={onReview}>Review</button>
+    </div><div className="Player-icon">
         <span className="user-icon">🔹</span>
         <span className="user-label">Player</span>
-      </div>
-    </>
+      </div></>
   );
 };
+
 export default ThankYouScreen;
