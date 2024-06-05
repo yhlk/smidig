f9a8196de4458e1e96017da9f871b5eaf76505d7
TariqEl <123506602+TariqEl@users.noreply.github.com>
2024-06-05 11:11:19 +0200

    Added dotenv file and mongodb config

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const questions = require("./routes/questions");
 const choices = require("./routes/choices");
 const results = require("./routes/results");
 const ratings = require("./routes/ratings");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,6 +19,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB here
+const mongoUrl = process.env.MONGO_URL;
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Use routes
 app.use("/api/users", users);
