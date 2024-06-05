0ffb2cc6ca38a9c739b34c39ef330588e97838f7
Mariam Kajo <Maryamkajo2001@hotmail.com>
2024-06-05 12:26:43 +0200

    Font sizes adjusted

--- frontend/src/components/ThankYouScreen.js
+++ frontend/src/components/ThankYouScreen.js
@@ -5,7 +5,7 @@ const ThankYouScreen = ({ onReview }) => {
   return (
     <><div className="thankyou-screen">
       <h1 className="title">Thank you for playing!</h1>
-      <h2 className="subtitle">Please rate your experience</h2>
+      <h2 className="subtitle">please rate your experience</h2>
       <button className="review-button" onClick={onReview}>Review</button>
     </div><div className="Player-icon">
         <span className="user-icon">🔹</span>
